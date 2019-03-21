e6f30272ee7422f0b1f686bb7063f40ce627d47c
Desmond O. Chang <dochang@gmail.com>
2019-03-21 22:03:22 +0800

    croc 4.1.5

--- Formula/croc.rb
+++ Formula/croc.rb
@@ -1,15 +1,14 @@
 class Croc < Formula
   desc "Easily and securely send things from one computer to another"
   homepage "https://schollz.com/software/croc/"
-  url "https://github.com/schollz/croc/archive/v4.1.4.tar.gz"
-  sha256 "0f81f22f6af478b0d499be4da19a38dff49aae2b9a4e1295f9949f15f185d863"
+  url "https://github.com/schollz/croc/archive/v4.1.5.tar.gz"
+  sha256 "a44cbe6d632f5962c7228ee61a852b5f156a3a43011eeaf8a8cd7c1aa482e2e5"
   head "https://github.com/schollz/croc.git"
 
   depends_on "go" => :build
 
   def install
     ENV["GOPATH"] = "#{HOMEBREW_CACHE}/go_cache"
-    ENV["CGO_ENABLED"] = "0"
 
     system "go", "build", "-o", "#{bin}/croc", "-ldflags", "-s -w -X main.Version=v#{version}"
     prefix.install_metafiles
